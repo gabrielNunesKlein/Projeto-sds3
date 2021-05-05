da0275f2cfa75b90e9a10a1909c42cf15872241f
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-05-05 17:49:35 -0300

    Group by search

--- backend/src/main/java/com/klein/dsvendas/repositories/SaleRepository.java
+++ backend/src/main/java/com/klein/dsvendas/repositories/SaleRepository.java
@@ -1,9 +1,22 @@
 package com.klein.dsvendas.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.klein.dsvendas.dto.SaleSuccessDTO;
+import com.klein.dsvendas.dto.SaleSumDTO;
 import com.klein.dsvendas.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long>{
 
+	@Query("SELECT new com.klein.dsvendas.dto.SaleSumDTO(obj.seller, SUM(obj.amount))"
+			+ " FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSumDTO> amountGroupBySeller();
+	
+	
+	@Query("SELECT new com.klein.dsvendas.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals))"
+			+ " FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSuccessDTO> successGroupBySeller();
 }
